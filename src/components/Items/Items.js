223ae7f06cf15d70bc611db7275d0982cc50314d
Sam Darmali <samdarmali@sams-mbp-2.lan>
2021-06-08 22:06:11 +0800

    realize that redux not needed, this branch will be used to transform to all redux to hooks

--- src/components/Items/Items.js
+++ src/components/Items/Items.js
@@ -1,43 +1,42 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Button } from '@material-ui/core';
-import { NavigateNext } from '@material-ui/icons';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Button } from "@material-ui/core";
+import { NavigateNext } from "@material-ui/icons";
 
-import classes from './Items.module.css';
-import NewItem from './NewItem/NewItem';
-import ItemList from './ItemList/ItemList';
-import Totals from './Totals/Totals';
-import * as actions from '../../store/actions/index';
-import Charges from './Charges/Charges';
+import "./Items.css";
+import NewItem from "./NewItem/NewItem";
+import ItemList from "./ItemList/ItemList";
+import Totals from "./Totals/Totals";
+import * as actions from "../../store/actions/index";
+import Charges from "./Charges/Charges";
 
-class Items extends Component {
+const Items = (props) => {
+    /* onNext() is mapped in dispatch below, calls action.next() REDUX */
+  const onNextHandler = () => {
+    props.onNext();
+  };
 
-    onNextHandler = () => {
-        this.props.onNext()
-    }
+  return (
+    <div className="Items">
+      <NewItem />
+      <ItemList itemObj={props.items} />
+      <Charges />
+      <Totals />
+      <div className="ButtonDiv">
+        <Button variant="contained" onClick={props.onNext}>
+          People
+          <NavigateNext />
+        </Button>
+      </div>
+    </div>
+  );
+};
 
-    render() {
-        return (
-            <div className={classes.Items}>
-                <NewItem />
-                <ItemList itemObj={this.props.items} />
-                <Charges />
-                <Totals />
-                <div className={classes.ButtonDiv}>
-                    <Button variant="contained" onClick={this.props.onNext}>
-                        People<NavigateNext />
-                    </Button>
-                </div>
-            </div>
-        )
-    }
-
-}
-
-const mapDispatchToProps = dispatch => {
-    return {
-        onNext: () => dispatch(actions.next())
-    }
-}
+/* REDUX */
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onNext: () => dispatch(actions.next()),
+  };
+};
 
 export default connect(null, mapDispatchToProps)(Items);
