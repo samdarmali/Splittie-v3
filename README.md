7015e19080d6794f45e6cc5bfe180a25e4378b3b
Sam Darmali <samdarmali@sams-mbp-2.lan>
2021-06-08 22:12:12 +0800

    Start of react with hooks

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-## Splittie V3
+## Splittie V3.1.0
 
 App to split bills with your friends, factoring in service and GST charges proportionately. 
 
-This app was built with React and Redux.
+This app was built with Functional Components and React Hooks.
--- src/containers/NewBill/NewBill.js
+++ src/containers/NewBill/NewBill.js
@@ -5,17 +5,6 @@ import Items from "../../components/Items/Items";
 import People from "../../components/People/People";
 import Breakdown from "../../components/Breakdown/Breakdown";
 
-// class NewBill extends Component {
-//     render() {
-//         switch (this.props.step) {
-//             case 1: return <Items items={this.props.items} />
-//             case 2: return <People people={this.props.people} />
-//             case 3: return <Breakdown />
-//             default: return null
-//         }
-//     }
-// }
-
 const NewBill = (props) => {
   switch (props.step) {
     case 1:
