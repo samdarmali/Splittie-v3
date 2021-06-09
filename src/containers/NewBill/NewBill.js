c7a10cacee01421abfe5fc1ecb00a3186dc3ad9d
Sam Darmali <samdarmali@sams-mbp-2.lan>
2021-06-09 22:00:52 +0800

    navigation changed to hooks

--- src/containers/NewBill/NewBill.js
+++ src/containers/NewBill/NewBill.js
@@ -1,21 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
+import { Button } from "@material-ui/core";
+import { NavigateNext, NavigateBefore } from "@material-ui/icons";
 
+import "./NewBill.css";
 import Items from "../../components/Items/Items";
 import People from "../../components/People/People";
 import Breakdown from "../../components/Breakdown/Breakdown";
 
 const NewBill = (props) => {
-  switch (props.step) {
-    case 1:
-      return <Items items={props.items} />;
-    case 2:
-      return <People people={props.people} />;
-    case 3:
-      return <Breakdown />;
-    default:
-      return null;
-  }
+  const [step, setStep] = useState(1);
+  const [items, setItems] = useState([]);
+  const [people, setPeople] = useState([]);
+  const [buttons] = useState({
+    1: ["", "People"],
+    2: ["Items", "Confirm"],
+    3: ["People", ""],
+  });
+
+  const onNextHandler = () => {
+    if (step < 3) {
+        setStep(step + 1);
+    }
+    console.log(buttons[step]);
+  };
+
+  const onPrevHandler = () => {
+    if (step > 1) {
+        setStep(step - 1);
+    }
+    console.log(buttons[step]);
+  };
+
+  return (
+    <div className="NewBill">
+      {step == 1 ? <Items items={items} step={step} /> : null}
+      {step == 2 ? <People people={people} step={step} /> : null}
+      {step == 3 ? <Breakdown /> : null}
+      <div className="ButtonDiv">
+        <Button variant="contained" onClick={onPrevHandler}>
+          <NavigateBefore />
+          {buttons[step][0]}
+        </Button>
+        <Button variant="contained" onClick={onNextHandler}>
+          {buttons[step][1]}
+          <NavigateNext />
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => {
@@ -26,4 +59,5 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(NewBill);
+// export default connect(mapStateToProps)(NewBill);
+export default NewBill;
